d3e11e5963c55cad712f052a32816f0558fe5c1c
ajwade <ajwade28@gmail.com>
2026-05-30 13:54:26 -0400

    Added an algorithm to find the shortest or longest chip pattern for a specified starting color to ending color

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChipSecuritySystem.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,56 @@ namespace ChipSecuritySystem
     {
         static void Main(string[] args)
         {
+            Color startColor = GetColor(args, "-startColor", "Blue");
+            Color endColor = GetColor(args, "-endColor", "Green");
+           
+            bool smallestList = !args.Contains("--longest") && !args.Contains("-l"); //smallestL
+
+            ChipService s = new ChipService();
+            List<ColorChip> list = s.GetTestChips();
+            Console.WriteLine("This is the list of bi-color chips:");
+
+            foreach(var item in list)
+            {
+               Console.WriteLine($"[{item.StartColor}, {item.EndColor}]");
+            }
+            Console.WriteLine("");
+            List<ColorChip> validCombinations = s.ValidCombinations(list, startColor, endColor, smallestList);
+            if (validCombinations == null)
+                Console.WriteLine(Constants.ErrorMessage);
+            else
+            {
+                Console.WriteLine($"Valid combination found from {startColor} to {endColor}.  The {(smallestList ? "shortest" : "longest")} combination is:");
+                foreach (var item in validCombinations)
+                    Console.WriteLine($"[{item.StartColor}, {item.EndColor}]");
+            }
+        }
+
+        private static Color GetColor(string[] args, string name, string defaultValue)
+        {
+            int index = Array.IndexOf(args, name);
+            string argValue = defaultValue;
+            if (index != -1 && index < args.Length - 1)
+            {
+                argValue = args[index + 1];
+            }
+            switch (argValue)
+            {
+                case "Red":
+                    return Color.Red;
+                case "Green":
+                    return Color.Green;
+                case "Blue":
+                    return Color.Blue;
+                case "Yellow":
+                    return Color.Yellow;
+                case "Orange":
+                    return Color.Orange;
+                case "Purple":
+                    return Color.Purple;
+                default:
+                    throw new Exception("Unknown color: " + argValue);
+            }
         }
     }
 }
